b6958c9f70d85d6764561b4e8cb6c9b9c7963f39
Anh Le <anh.le@beteasy.com>
2019-01-03 23:47:52 +1100

    create a simple reducer function and a test

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,40 @@
+import { createInitialState } from '../utils/createInitialState'
+import createStore from '../utils/createStore'
+import { createAddTodo, createInitAction } from '../actions/todoActions'
+
+export const reducer = (prevState = createInitialState(), action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            console.log('[DEBUG]')
+            const nextState = {
+                todoList: [
+                    ...prevState.todoList,
+                    action.text,
+                ],
+            };
+
+            return nextState
+        default:
+            return prevState
+    }
+}
+
+// Can make some console logs for verification
+export const testStore = () => {
+
+    // create & init store with initial state and action
+    const initAction = createInitAction()
+    const initialState = createInitialState()
+    const store = createStore(reducer, initialState)
+    store.dispatch(initAction)
+
+    // confirm state
+    console.log(JSON.stringify(store.getState()))
+
+    // dispatch add TO-DO action
+    const addToDoAction = createAddTodo('Build a simple redux store')
+    store.dispatch(addToDoAction)
+
+    // confirm state
+    console.log(JSON.stringify(store.getState()))
+}
