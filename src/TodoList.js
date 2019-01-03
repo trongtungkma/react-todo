bbad2f061db763522ed6034c8e090911dc01f7ec
Anh Le <anh.le@beteasy.com>
2019-01-03 21:22:51 +1100

    feat: Convert TodoList into functional component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 
-class TodoList extends Component {
-  createTasks = item => {
+const TodoList = ({ tasks }) => {
+
+  const createTasks = item => {
     return (
       <li key={item.key}>
         {item.text}
       </li>
     )
   }
-  render() {
-    const { tasks } = this.props;
-    const listTasks = tasks.map(this.createTasks)
 
-    return <ul className="theList">{listTasks}</ul>
-  }
+  const listTasks = tasks.map(createTasks)
+
+  return (
+      <ul className="theList">
+        {listTasks}
+      </ul>
+  )
 }
 
 export default TodoList
