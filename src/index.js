35072b30082ba327d27cb6be398bd539dc67cec0
Anh Le <anh.le@beteasy.com>
2019-01-03 23:48:34 +1100

    feat: Adding test invocation of redux in the index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { testStore } from './reducers/todoReducer'
 
+// verify our store, check output in your console
+testStore()
 ReactDOM.render(<App />, document.getElementById('root'));
 
