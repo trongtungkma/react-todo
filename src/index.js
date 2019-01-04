7041b2a20e8114bf0723b9731a29b3c90889a7a2
Anh Le <anh.le@beteasy.com>
2019-01-04 16:10:23 +1100

    feat: Create root reducer and renaming reducer function.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import createStore from './utils/createStore'
-import { reducer } from './reducers/todoReducer'
+import { todoReducer } from './reducers/todoReducer'
 import { createInitialState } from './utils/createInitialState'
 
-const store = createStore(reducer, createInitialState())
+const store = createStore(todoReducer, createInitialState())
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,7 +3,7 @@ import createStore from '../utils/createStore'
 import { createAddTodo } from '../actions/todoActions'
 import { ADD_TODO } from '../constants/actions'
 
-export const reducer = (prevState = createInitialState(), action) => {
+export const todoReducer = (prevState = createInitialState(), action) => {
     switch (action.type) {
         case ADD_TODO:
             const nextState = {
@@ -17,22 +17,4 @@ export const reducer = (prevState = createInitialState(), action) => {
         default:
             return prevState
     }
-}
-
-// Can make some console logs for verification
-export const testStore = () => {
-
-    // create & init store with initial state and action
-    const initialState = createInitialState()
-    const store = createStore(reducer, initialState)
-
-    // confirm state
-    console.log(JSON.stringify(store.getState()))
-
-    // dispatch add TO-DO action
-    const addToDoAction = createAddTodo('Build a simple redux store')
-    store.dispatch(addToDoAction)
-
-    // confirm state
-    console.log(JSON.stringify(store.getState()))
 }
