d6940739d2bee03b96f3553b62bcdf50458b4212
Anh Le <anh.le@beteasy.com>
2019-01-05 22:22:23 +1100

    feat: Adding label for Task menu.

--- src/components/TodoMenu/TodoMenu.js
+++ src/components/TodoMenu/TodoMenu.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import {Menu, Input} from 'semantic-ui-react'
+import {Menu, Input, Label} from 'semantic-ui-react'
 
 const TodoMenu = () =>  (
     <Menu pointing>
-        <Menu.Item name='home' active={true} />
-        <Menu.Item
-            name='Active'
-        />
-        <Menu.Item
-            name='Archive'
-        />
+        <Menu.Item name='home' active={true}>
+            Home
+            <Label color='teal'>0</Label>
+        </Menu.Item>
+        <Menu.Item name='archive'>
+            Archive
+            <Label color='yellow'>10</Label>
+        </Menu.Item>
+        <Menu.Item name='inactive'>
+            Inactive
+            <Label color='red'>20</Label>
+        </Menu.Item>
         <Menu.Menu position='right'>
             <Menu.Item>
                 <Input icon='search' placeholder='Search task' />
